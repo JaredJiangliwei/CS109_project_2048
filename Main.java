6fc5fa9da3cb10eb7c96a6ee2adc20f13666293c
JaredJiangliwei <12310807@mail.sustech.edu.cn>
2024-05-25 23:49:48 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-package project;
-
-
 public class Main {
     public static void main(String[] args) {
         // 创建GameFrame的实例，并设置窗口大小
-        GameFrame gameFrame = new GameFrame(600, 400);
+        GameLoginInterface gameLoginInterface = new GameLoginInterface();
+        // 创建一个新的线程播放音乐，背景音乐独立于游戏逻辑的执行，会一直播放，直到程序终止
+        new Thread(BGM::playBackgroundMusic).start();
 
         // 设置窗口为可见状态
-        gameFrame.setVisible(true);
+        gameLoginInterface.display();
     }
 }
-
-
