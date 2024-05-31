b595a6959d0598b2e3ecde165318d2e70a24d06e
1notsleepy <1552041366@qq.com>
2024-05-31 21:45:22 +0800

    Update ColorMap.java

--- ColorMap.java
+++ ColorMap.java
@@ -1,4 +1,5 @@
-package project;
+package util;
+
 
 import java.awt.*;
 import java.util.HashMap;
@@ -19,24 +20,26 @@ public class ColorMap {
     //通过调用 InitialColorMap 方法，你可以为 colorMap 添加键值对，从而定义颜色映射。
     public static void InitialColorMap() {
         colorMap.put(1, Color.RED);//红
-        colorMap.put(2, Color.ORANGE);//橙
-        colorMap.put(4, Color.YELLOW); // 黄
-        colorMap.put(8, Color.GREEN); // 绿
-        colorMap.put(16, new Color(0, 255, 255)); // 青
-        colorMap.put(32, Color.BLUE); // 蓝
-        colorMap.put(64, new Color(255, 0, 255)); // 紫
-        colorMap.put(128, new Color(128, 255, 128)) ;// 淡绿色
-        colorMap.put(256, Color.PINK); // 粉
-        colorMap.put(512, Color.GRAY); // 灰
-        colorMap.put(1024, new Color(128, 0, 0)); // 暗红色
-        colorMap.put(2048, new Color(192, 192, 192)); // 灰白色
+        colorMap.put(2, new Color(238,228, 218));
+        colorMap.put(4, new Color(235, 218, 188));
+        colorMap.put(8, new Color(241, 177, 121));
+        colorMap.put(16, new Color(246, 145, 94));
+        colorMap.put(32, new Color(210, 105, 30));
+        colorMap.put(64, new Color(243, 81, 44));
+        colorMap.put(128, new Color(237, 205, 113)) ;
+        colorMap.put(256,new Color(45, 130, 128));
+        colorMap.put(512, new Color(9, 97, 26));
+        colorMap.put(1024, new Color(237, 205, 14));
+        colorMap.put(2048, new Color(192, 20, 20));
         colorMap.put(4096, Color.CYAN); // 蓝绿
-        colorMap.put(8192, new Color(255, 255, 128)); // 淡橙色
+        colorMap.put(8192, new Color(255, 255, 128));
     }
-//getOrDefault 方法是 HashMap 类的一个方法，用于从映射中获取指定的键的值，如果不存在该键，则返回默认值。
+    //getOrDefault 方法是 HashMap 类的一个方法，用于从映射中获取指定的键的值，如果不存在该键，则返回默认值。
 //
 //在 ColorMap 类中，getColor 方法应该返回与整数键 i 关联的颜色，如果不存在，则返回默认颜色 Color.black。
     public static Color getColor(int i) {
         return colorMap.getOrDefault(i, Color.black);
     }
 }
+
+
