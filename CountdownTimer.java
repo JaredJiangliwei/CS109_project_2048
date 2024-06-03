82624fe757028edd822219c9a0aeccb130b92045
1notsleepy <1552041366@qq.com>
2024-06-03 11:32:20 +0800

    Create CountdownTimer.java

---
+++ CountdownTimer.java
@@ -0,0 +1,23 @@
+package view;
+
+public class CountdownTimer {
+    private int seconds;
+
+    public CountdownTimer(int totalSeconds) {
+        this.seconds = totalSeconds;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void decrementSeconds() {
+        if (seconds > 0) {
+            seconds--;
+        }
+    }
+
+    public boolean isFinished() {
+        return seconds <= 0;
+    }
+}
